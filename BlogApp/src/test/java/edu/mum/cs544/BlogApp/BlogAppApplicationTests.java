9de0547f5b8c1d1445e33bf99c67a8b2eed87f25
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2022-09-23 14:03:50 -0500

    the prototype for the project is created

---
+++ BlogApp/src/test/java/edu/mum/cs544/BlogApp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs544.BlogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ post/src/test/java/edu/mum/cs544/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs544.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
